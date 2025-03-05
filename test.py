19275664dc5c5e55a754f837610b81f7b299d878
arshiabyya <abhatt21@ucsc.edu>
2025-03-05 00:30:11 -0800

    Reading classes csv file

--- test.py
+++ test.py
@@ -13,58 +13,9 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-transform = transforms.Compose([
-        transforms.ToTensor(),
-        # put other transforms here! https://pytorch.org/vision/stable/transforms.html#v2-api-ref
-        # some useful ones might be resizing or cropping, consider also doing random augmentations
-        # to make your model more robust.
-    ])
-
-dataset_path = "./train"
-
-dataset = datasets.ImageFolder(root=dataset_path, transform=transform)
-
-print(len(dataset))
-
-class ImageFolderDataset(Dataset):
-    def __init__(self, image_paths):
-        # here, you should set any important vairables you want to use in __len__ or __getitem__
-        self.image_paths = image_paths
-
-        
+import pandas as pd
 
 
+dataset = pd.read_csv("_classes.csv")
 
-
-    # # this should just return the length of the dataset, without processing or opening any images
-    # def __len__(self):
-    #     return len(image_paths)
-
-    # def __getitem__(self, idx):
-
-    #     # we get the image path from the index
-    #     image_path = image_paths[idx]
-
-    #     # use the PIL class to get the image
-    #     image = Image.open(image_path).convert('RGB')
-
-    #     # run transformations on image
-    #     image = self.transform(image)
-
-    #     return image
-    
-# image_paths = ["./test.png"]
-
-# # make or dataset class
-# dataset = ImageFolderDataset(image_paths)
-
-# # Create a dataloader, we can make a batch size and shuffle the image order
-# dataloader = DataLoader(dataset, batch_size=128, shuffle=True)
-
-# # Process the images
-# for batch in dataloader:
-#     # 'batch' is now a tensor of shape (batch_size, 3, 224, 224)
-#     # You can perform further operations on this batch of tensors
-#     print(f"Processed batch shape: {batch.shape}")
-
-# print(f"Total number of images processed: {len(dataset)}")
+print(dataset.info())
